ffedaf32a09236299df88c6f1806c50ae46c837e
yudong <wangyuuudong@gmail.com>
2024-04-20 22:25:09 +0800

    init

---
+++ main.js
@@ -0,0 +1,85 @@
+const robot = require('robotjs');
+const pos = require('./pos');
+
+const SingleClick = false;
+const DoubleClick = true;
+const {
+  Servers,
+  Channel,
+  Character,
+  MenuOpen,
+  MenuTaskIcon,
+  MenuTaskAccept,
+  MenuTaskComplete,
+} = pos;
+const sel = process.env.SERVER_SEL;
+
+console.log(sel);
+
+robot.setMouseDelay(100);
+robot.setKeyboardDelay(100);
+
+const main = async () => {
+  await sleep(5000);
+  robot.keyTap('left', 'win');
+  console.log(`server ${sel} start`);
+  robot.moveMouse(Servers[sel].x, Servers[sel].y);
+  robot.mouseClick('left', SingleClick);
+  await sleep(3000);
+  robot.moveMouse(Channel.x, Channel.y);
+  robot.mouseClick('left', DoubleClick);
+  await sleep(5000);
+  robot.moveMouse(Character.x, Character.y);
+  robot.mouseClick('left', DoubleClick);
+  await sleep(10000);
+  keyTap('escape');
+  await sleep(3000);
+  robot.moveMouse(MenuOpen.x, MenuOpen.y);
+  robot.mouseClick('left', SingleClick);
+  await sleep(3000);
+
+  for (let i = 0; i < 5; i++) {
+    robot.moveMouse(MenuTaskIcon.x, MenuTaskIcon.y);
+    robot.mouseClick('left', SingleClick);
+    await sleep(3000);
+    robot.moveMouse(MenuTaskAccept.x, MenuTaskAccept.y);
+    robot.mouseClick('left', SingleClick);
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(930000);
+    robot.moveMouse(MenuTaskIcon.x, MenuTaskIcon.y);
+    robot.mouseClick('left', SingleClick);
+    await sleep(3000);
+    robot.moveMouse(MenuTaskComplete.x, MenuTaskComplete.y);
+    robot.mouseClick('left', SingleClick);
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(3000);
+    robot.keyTap('space');
+    await sleep(3000);
+  }
+
+  robot.keyTap('escape');
+  await sleep(3000);
+  robot.keyTap('up');
+  await sleep(3000);
+  robot.keyTap('enter');
+  await sleep(3000);
+  robot.keyTap('enter');
+  await sleep(5000);
+  robot.keyTap('escape');
+  console.log(`server ${sel} end`);
+}
+
+function sleep(ms) {
+  console.log(`sleep ${ms}` ms);
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
---
+++ pos.js
@@ -0,0 +1,16 @@
+module.exports = {
+  Servers: [
+    {x: 940, y: 70 },
+    {x: 940, y: 100 },
+    {x: 940, y: 130 },
+    {x: 940, y: 170 },
+    {x: 940, y: 200 },
+    {x: 940, y: 230 }
+  ],
+  Channel: {x: 514, y: 337 },
+  Character: {x: 130, y: 344 },
+  MenuOpen: { x: 34, y: 364 }, // 黄色星星图标
+  MenuTaskIcon: { x: 268, y: 364 }, // 美好世界图标
+  MenuTaskAccept: { x: 920, y: 534 }, // 小小的幸运，开始任务后按 3 次空格
+  MenuTaskComplete: { x: 920, y: 490 }, // 完成任务后按 4 次空格
+};
